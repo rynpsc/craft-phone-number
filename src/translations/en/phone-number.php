18b5e9e3e007e559d6fa1980d9a6dfdca76e7e93
Ryan <ryan.pascoe@outlook.com>
2023-12-18 12:16:46 +0000

    Update translations

--- src/translations/en/phone-number.php
+++ src/translations/en/phone-number.php
@@ -6,9 +6,14 @@
  */
 
 return [
+    'E164' => 'E164',
+    'How field values will be formatted in element indexes and card views.' => 'How field values will be formatted in element indexes and card views.',
+    'International' => 'International',
+    'National' => 'National',
     'No results' => 'No results',
+    'None' => 'None',
+    'Number' => 'Number',
     'Phone Number' => 'Phone Number',
     'Region' => 'Region',
     'The phone number provided is invalid.' => 'The phone number provided is invalid.',
-    'How field values will be formatted in element indexes and card views.' => 'How field values will be formatted in element indexes and card views.',
 ];
